f8fe63655548e9fe960cbe9cf1a9d3ab337dbebb
Le-Roy <jongwele@gmail.com>
2025-01-10 20:57:33 +0200

    modified

--- src/main/resources/static/js/client/client-registration.js
+++ src/main/resources/static/js/client/client-registration.js
@@ -43,7 +43,7 @@ let clientRegistration = {
 
     //Post client data to server
     registerClient: () => {
-        fetch(`/client/management/register/${userData.companyId}`, {
+        fetch(`/client/management/register`, {
             method: 'POST',
             headers:{
                 'Content-Type': 'application/json',
